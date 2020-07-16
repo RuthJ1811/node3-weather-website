a444ecf5c79bb9bcca2d6a298cd139d793884dec
Apple <apple@Apples-MacBook-Air.local>
2020-07-16 23:34:55 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+// absolute path - /js/app.js / will take to root folder, that is public
+
+console.log('Client side js is loaded')
+
+
+const weatherForm = document.querySelector('form') // to return the json on form tag
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = "Please wait while I fetch weather details"
+    messageTwo.textContent = ""
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,121 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPaths = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+// inform express to set handlebar and hbs wants all the files to be stored in views folder in the root directory
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPaths)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+// now we need to use views folder for hbs, hbs will automatically identify views folder
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Hbs training',
+        name: 'Ruth'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Ruth'
+    }) // file name
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Ruth'
+    })
+})
+
+
+// NOW WE SET UP A SERVER TO TELL WHAT TO HAPPEN IF USER GOES TO THE SPECIFIED URL
+// WE WILL REMOVE THIS DEFAULT SINCE IT IS HANDLED IN INDEX.HTML
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>')
+// })
+
+// WE WILL BE REMOVING THE ROUTES for about and help, BECAUSE APP.USE(EXPRESS.STATIC(DIR NAME) IS POINTING TO PUBLIC, AND IT WILL TAKE THE RESPECTIVE FILES) 
+
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            'error': 'you must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            })
+        }
+        forecast(latitude, longitude, (error, data) => {
+            if (error) {
+                return res.send({
+                    error
+                })
+            }
+
+            res.send({
+                location,
+                forecast: data,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            'error': 'You must provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        'products': []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Ruth'
+    })
+})
+
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Ruth'
+    })
+})
+
+
+// USE LISTEN TO START UP THE SERVER
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
